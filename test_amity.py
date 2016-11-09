9619b82d93b928a67b0968f07ba6218583081f03
Gacheru <evans.gacheru@andela.com>
2016-11-09 10:43:00 +0300

    [Tests] add tests for amity

--- test_amity.py
+++ test_amity.py
@@ -1,6 +1,7 @@
 import unittest
 
-from room import Room
+from tests.person import Person
+from tests.room import Room
 
 
 class AmityTest(unittest.TestCase):
@@ -9,13 +10,32 @@ class AmityTest(unittest.TestCase):
         self.room = Room()
 
     def test_look_up_room(self):
-        self.room.add('Living Space', 'Python')
-        self.assertEqual('Python', self.room.lookup('Living Space'))
+        self.room.add('Living Space', 'Python','Office')
+        self.assertEqual('Python', self.room.lookup('Living Space', 'Office'))
+
+    def test_normal_entries_is_consistent(self):
+        self.room.add('Living Space', 'Go')
+        self.room.add('Living Space', 'PHP')
+        self.room.add('Office Space', 'valhalla')
+        self.assertTrue(self.room.is_consistent())
+
+    def test_adds_roomType_roomName(self):
+        self.room.add('Living Space')
+
+    def test_livingroom_max_occupancy(self):
+        pass
+
 
     def test_missing_entry_raises_KeyError(self):
         with self.assertRaises(KeyError):
             self.room.lookup('Missing')
 
+    def test_empty_room(self):
+        self.assertTrue(self.room.is_office())
+
+    def tearDown(self):
+        pass
+
+
 if __name__ == '__main__':
     unittest.main()
-
---
+++ tests/person.py
@@ -0,0 +1,12 @@
+class Person(object):
+    def __init__(self):
+        self.entries = {}
+
+    def add(self, name, room):
+        pass
+
+    def lookup(self, state):
+        pass
+
+    def is_staff(self):
+        pass
