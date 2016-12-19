11696c6da207bc5ea619d9100a46c1ef676e063e
Evans Gacheru <evans.gacheru@andela.com>
2016-12-19 15:22:55 +0300

    [Feature]Added new functions for allocation and unallocated records.

--- app/rooms/roomClass.py
+++ app/rooms/roomClass.py
@@ -32,4 +32,73 @@ class Room(object):
 
         return message
 
-    
+    def print_allocations(self, args):
+        """ print rooms and occupants"""
+
+        data = ""
+        for room in rooms:
+            room_info = rooms.get(room,None)
+            room_type = "Living Space"
+
+            if room_info["is_office"]:
+                room_type = "Office"
+            data += "\n\n{} ({}) \n".format(room.upper(), room_type)
+            data += "-" * 50
+            data += "\n"
+
+            if len(rooms[room]["occupants"]) is 0:
+                data += "No Occupants"
+            occupants = map(self.get_names, rooms[room]["occupants"])
+            data +=  "\n".join(occupants)
+
+        if args["-o"]:
+            with open(args["<filename>"], "wt") as output_file:
+                output_file.write(data)
+                print ("Allocations has been saved to {}".format(args["filename"]))
+        return data
+
+    def print_unallocated(self, args):
+        """Check if person has been allocated a office if not prints out name, checks if person who wants accommodation
+         has been allocated has been allocated living space if not prints out name"""
+        data = ""
+
+        office_allocations = []
+        living_space_allocations = []
+        people_without_living_spaces = []
+
+        for room_name, room_info in rooms.items():
+            if room_info["is_office"]:
+                office_allocations += room_info["occupants"]
+            if not room_info["is_office"]:
+                living_space_allocations += room_info["occupants"]
+
+        unllocated_offices = list(set(people_data.keys()) - set(office_allocations))
+        people_without_offices = list(map(self.get_names, unllocated_offices))
+
+        data += "Those unallocated Offices: \n"
+        if len(people_without_offices):
+            data +="\n".join(people_without_offices)
+        else:
+            data += "NONE"
+
+        data += "\n\nThose unallocated living spaces:\n"
+
+        for person_id, person_info in people_data.items():
+            if person_info["is_fellow"] and person_info["accommodation"] == "Y":
+                if person_id not in living_space_allocations:
+                    people_without_living_spaces.append(person_info["name"])
+
+        if len(people_without_living_spaces):
+            data += "\n".join(people_without_living_spaces)
+        else:
+            data += "NONE"
+
+        if args["-o"]:
+            with open(args["<filename>"], "wt") as output_file:
+                output_file.write(data)
+                print ("Unallocated people have been saved to {}".format(args["<filename>"]))
+        return data
+
+
+
+
