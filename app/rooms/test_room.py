818a14eb423d7cea06678f5685835284bf258f1e
Evans Gacheru <evans.gacheru@andela.com>
2016-12-19 08:31:24 +0300

    [Feature] Added new function tests in test rooms.

--- app/rooms/test_room.py
+++ app/rooms/test_room.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import unittest
 import mock
-import roomClass import Room
+from roomClass import Room
 
 class TestRoom(unittest.TestCase):
     """ """
@@ -17,4 +17,49 @@ class TestRoom(unittest.TestCase):
         self.assertEqual(self.get_name, "EVANS GACHERU", msg = "Wrong Person name retrieved.")
         self.assertEqual(self.get_name_with_wrong_id, "Person Does not exist", msg= "Person Exists")
 
-    @mock.patch.dict("amity.amityClass.rooms", {})
+    @mock.patch.dict("amity.amityClass.rooms", {
+        "VALHALLA":{"is_office":True, "occupants":[2]},
+        "HOGWARTS":{"is_office":True, "occupants":[3]},
+        "OCULUS":{"is_office":True, "occupants":[4]},
+        "PYTHON":{"is_office":False, "occupants":[2]}
+    })
+    @mock.patch.dict("personClass.people_data", {
+        1:{"name": "EVANS GACHERU", "is_fellow": True,"accommodation": "Y" },
+        2:{"name": "Evans MUSOMI", "is_fellow": False, "accommodation":"N"}})
+    @mock.patch("roomClass.open")
+    def test_print_allocations(self,mocked_open):
+        #Test Print allocations function
+        self.print_allocations_without_filename = self.test_room.print_allocations({
+            "-o": False, "<filename>": None})
+        self.test_room.print_allocations({
+            "-o": True, "<filename>":"test_allocations.txt"})
+        mocked_open.assert_called_once_with("test_allocations.txt", "wt")
+        self.assertNotEqual(self.print_allocations_without_filename, "",msg = "Wrong data printed ")
+
+    @mock.patch.dict("amity.amityClass.rooms", {
+        "KRYPTON":{"is_office" : True, "occupants" : []}})
+    @mock.patch.dict("person.personClass.people_data", {
+        1:{"name":"EVANS GACHERU","is_fellow": True, "accommodation" : "Y"}})
+    @mock.patch("roomClass.open")
+    def test_print_unallocated(self, mocked_open):
+        #Test print unallocated
+        self.print_unallocated_without_filename = self.test_room.print_unallocated({
+            "-o": False, "<filename>":None})
+        self.test_room.print_unallocated({
+            "-o":True, "<filename>":"test_unallocated.txt"})
+        self.assertIn("EVANS GACHERU", self.print_unallocated_without_filename, msg ="Wrong data printed")
+        mocked_open.assert_called_once_with("test_unallocated.txt", "wt")
+
+    @mock.patch.dict("amity.amityClass.rooms",{
+        "PYTHON":{"is_office":False , "occupants":[2]},
+        "GO":{"is_office":False, "occupants":[]}})
+    @mock.patch.dict("person.personClass.people_date", {
+        1:{"name": "EVANS GACHERU", "is_fellow":True, "accommodation":"Y"}})
+    def test_print_room(self):
+        #Test print rooms
+        self.printing_non_existent_room = self.test_room.print_room({"<room_name>": "LONGONOT"})
+        self.assertEqual(self.printing_non_existent_room, "LONGONOT Does NOT Exist")
+        self.printing_existent_room = self.test_room.print_room({"<room_name>": "PYTHON"})
+        self.assertNotEquals(self.printing_existent_room, "", msg = "Room Does Not Exist")
+        self.printing_empty_room = self.test_room.print_room({"<room_name>":"GO"})
+        self.assertIn("No Occupants", self.printing_empty_room, msg = "Room has no occupants")
