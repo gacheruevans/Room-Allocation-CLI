f4918e66cb3a9aecc244500913fe2c8d8b649c1d
Evans Gacheru <evans.gacheru@andela.com>
2017-01-16 23:25:24 +0300

    [fix]update amity test file

--- app/amity/test_amity.py
+++ app/amity/test_amity.py
@@ -167,8 +167,8 @@ class TestAmity(unittest.TestCase):
         1: {"name": "EVANS GACHERU", "is_fellow": True, "accommodation": "Y"}})
     def test_getting_persons_name_from_people_data(self):
         # Test's whether the record being sought for is in people data
-        self.get_name = self.test_room.get_names(1)
-        self.get_name_with_wrong_id = self.test_room.get_names(8)
+        self.get_name = self.test_amity.get_names(1)
+        self.get_name_with_wrong_id = self.test_amity.get_names(8)
         self.assertEqual(self.get_name, "EVANS GACHERU",
                          msg="Wrong Person name retrieved.")
         self.assertEqual(self.get_name_with_wrong_id,
@@ -186,9 +186,9 @@ class TestAmity(unittest.TestCase):
     @mock.patch("app.rooms.roomClass.open")
     def test_print_allocations(self, mocked_open):
         # Test Print allocations function
-        self.print_allocations_without_filename = self.test_room.print_allocations({
+        self.print_allocations_without_filename = self.test_amity.print_allocations({
             "-o": False, "<filename>": None})
-        self.test_room.print_allocations({
+        self.test_amity.print_allocations({
             "-o": True, "<filename>": "test_allocations.txt"})
         mocked_open.assert_called_once_with("test_allocations.txt", "wt")
         self.assertNotEqual(
@@ -201,9 +201,9 @@ class TestAmity(unittest.TestCase):
     @mock.patch("app.rooms.roomClass.open")
     def test_print_unallocated(self, mocked_open):
         # Test print unallocated
-        self.print_unallocated_without_filename = self.test_room.print_unallocated({
+        self.print_unallocated_without_filename = self.test_amity.print_unallocated({
             "-o": False, "<filename>": None})
-        self.test_room.print_unallocated({
+        self.test_amity.print_unallocated({
             "-o": True, "<filename>": "test_unallocated.txt"})
         self.assertIn(
             "EVANS GACHERU", self.print_unallocated_without_filename, msg="Wrong data printed")
@@ -216,15 +216,15 @@ class TestAmity(unittest.TestCase):
         1: {"name": "EVANS GACHERU", "is_fellow": True, "accommodation": "Y"}})
     def test_print_room(self):
         # Test print rooms
-        self.printing_non_existent_room = self.test_room.print_room(
+        self.printing_non_existent_room = self.test_amity.print_room(
             {"<room_name>": "LONGONOT"})
         self.assertEqual(self.printing_non_existent_room,
                          "LONGONOT Room Does Not Exist")
-        self.printing_existent_room = self.test_room.print_room(
+        self.printing_existent_room = self.test_amity.print_room(
             {"<room_name>": "PYTHON"})
         self.assertNotEqual(self.printing_existent_room,
                             "", msg="Room Does Not Exist")
-        self.printing_empty_room = self.test_room.print_room(
+        self.printing_empty_room = self.test_amity.print_room(
             {"<room_name>": "GO"})
         self.assertIn("No Occupants", self.printing_empty_room,
                       msg="Room has no occupants")
